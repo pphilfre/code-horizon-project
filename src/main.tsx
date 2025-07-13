fce455a41916c505be3744c892b5d434ff113ba9
Freddie philpot <pphilfre@monkswalk.herts.sch.uk>
2025-07-13 19:20:30 +0000

    feat: Integrate PostHog for analytics tracking in the application

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,20 @@
 import { createRoot } from 'react-dom/client'
+import { PostHogProvider } from 'posthog-js/react'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        defaults: '2025-05-24',
+        capture_exceptions: true,
+        debug: import.meta.env.MODE === 'development',
+      }}
+    >
+      <App />
+    </PostHogProvider>
+  </React.StrictMode>
+)
